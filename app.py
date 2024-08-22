7dc8874b697bb08b86b5daab1d2d375fd6a3d1dc
Nithin G <79520840+2nithing@users.noreply.github.com>
2024-08-22 06:47:00 +0000

    message

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.header('Calculator App')
+st.write('This is a calculator application developed using Streamlit')
+btn = st.button('Click Me')
+if btn:
+    st.balloons()
+
+num1 = st.number_input('Enter the first number',value=0)
+option = st.selectbox('Select the operation',("+","-","/","*"))
+num2 = st.number_input('Enter the second number',value=0)
+btn2 = st.button('Calculate')
+if btn2:
+    if option=="+":
+        st.subheader(f'Added result is {num1+num2}')
+    elif option=="-":
+        st.subheader(num1-num2)
+    elif option=="*":
+        st.subheader(num1*num2)
+    elif option=="/":
+        st.subheader(num1/num2)
